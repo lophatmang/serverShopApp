a472d58f0dff92747cca97793b42db8d514cc02d
LoPhatMang <thanhntfx32690@funix.edu.vn>
2024-11-22 20:15:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,18 @@ app.use(cartRouter);
 app.use(orderRouter);
 app.use(sessionRouter);
 app.use("/admin", adminRouter);
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
+
 app.use((req, res, next) => {
   res.status(404).json({
     errorMessage: "Route not found",
