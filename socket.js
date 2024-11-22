7d600f1b2adb46531ceec9021e002d42ba8fc7b3
LoPhatMang <thanhntfx32690@funix.edu.vn>
2024-11-22 20:23:20 +0700

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,6 +3,9 @@ let io;
 module.exports = {
   init: (httpServer) => {
     io = require("socket.io")(httpServer, {
+      cors: {
+        origin: "*",
+      },
       allowRequest: (req, callback) => {
         const noOriginHeader = req.headers.origin === undefined;
         callback(null, noOriginHeader);
